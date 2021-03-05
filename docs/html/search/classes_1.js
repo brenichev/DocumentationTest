51c7174bcbcce5fc812335e47b171528f98cc424
brenichev <63112367+brenichev@users.noreply.github.com>
2021-03-06 03:41:38 +0500

    r1

--- docs/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['queueclass_17',['QueueClass',['../class_documentation_test_1_1_queue_class.html',1,'DocumentationTest']]]
-];
--- docs/html/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['queueclass_28',['QueueClass',['../class_documentation_test_1_1_queue_class.html#a0225afa4fdcd6f1267e59de6adeab8cd',1,'DocumentationTest.QueueClass.QueueClass()'],['../class_documentation_test_1_1_queue_class.html#acf2aa40b992aa324d99eca1a63cd6c6a',1,'DocumentationTest.QueueClass.QueueClass(int capacity)'],['../class_documentation_test_1_1_queue_class.html#a91c64f2b4bd9945eab5c3164ec9f7636',1,'DocumentationTest.QueueClass.QueueClass(QueueClass&lt; T &gt; queue)']]]
-];
